c5dd3e71ca227e8d8c0a63380b2482c63043ea13
RaphGamingz <52985774+RaphGamingz@users.noreply.github.com>
2021-06-13 20:19:46 +0800

    Delete dist directory

--- dist/dataTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dataTypes = void 0;
-exports.dataTypes = {
-    locations: ["camp", "weaponry", "forge", "market"]
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGF0YVR5cGVzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2RhdGFUeXBlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBVyxRQUFBLFNBQVMsR0FBRztJQUNuQixTQUFTLEVBQUUsQ0FBQyxNQUFNLEVBQUUsVUFBVSxFQUFFLE9BQU8sRUFBRSxRQUFRLENBQUM7Q0FDckQsQ0FBQSJ9
--- dist/models/itemModel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.itemModel = void 0;
-class itemModel {
-}
-exports.itemModel = itemModel;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXRlbU1vZGVsLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL21vZGVscy9pdGVtTW9kZWwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBVUEsTUFBYSxTQUFTO0NBRXJCO0FBRkQsOEJBRUMifQ==
